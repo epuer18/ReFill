3e574a83c2db5e54a70484ec4d6089dff423d5f0
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-05 00:12:03 +0000

    Finish the server DB connection

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./component/header";
+import Login from "./component/login";
+import Register from "./component/register";
+import Home from "./component/home";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
