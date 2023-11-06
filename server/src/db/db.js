95df9ee801341e7a4477784e65c213355b95bffe
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-06 20:23:34 +0000

    URL encode mongoDB uri

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,25 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const url =
-//   "mongodb+srv://qiwenxin98:Zjjxwjp@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(url);
-
-// async function connectDB() {
-//   try {
-//     await client.connect();
-//     return client.db("FurFriendz");
-//   } catch (error) {
-//     throw new Error("Failed to connect to the database: " + error.message);
-//   }
-// }
-
-// module.exports.client = client;
-// module.exports.connectDB = connectDB;
+const username = encodeURIComponent("qiwenxin98");
+const password = encodeURIComponent("Zjjxwjp");
 
 const connectDB = async (collectionChosen) => {
   const mongoUrl =
-    "mongodb+srv://qiwenxin98:Zjjxwjp@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://${username}:${password}@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
   const client = new MongoClient(mongoUrl);
   await client.connect();
   const db = await client.db("Refill");
