04b052a3b1f87baacbbb39dab564111eb7049652
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-06 23:32:57 +0000

    Finish the server DB connection

---
+++ server/src/controller/userController.js
@@ -0,0 +1,43 @@
+import connectDB from "../db/db.js";
+import { ObjectId } from "mongodb";
+
+
+function userDb() {
+  const me = {};
+  me.createUser = async (user) => {
+    const { client, collection } = await connectDB("users");
+    const res = await collection.insertOne({
+      username: user.username,
+      password: user.password,
+    });
+    try {
+      return res;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.getUserByID = async (id) => {
+    const { client, collection } = await connectDB("users");
+    const res = await collection.findOne({ _id: new ObjectId(id) });
+    try {
+      return res;
+    } finally {
+      await client.close();
+    }
+  };
+
+  me.findUserByUsername = async (username) => {
+    const { client, collection } = await connectDB("users");
+    const res = await collection.findOne(username);
+    try {
+      return res;
+    } finally {
+      await client.close();
+    }
+  };
+
+  return me;
+}
+
+export const userDB = userDb();
---
+++ server/src/route/userRoute.js
@@ -0,0 +1,92 @@
+import express from "express";
+import { userDB } from "../controller/userController.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userRouter = express.Router();
+const JWT_SECRET = "sdjkfh8923yhjdksbfma@";
+
+userRouter.get("/login", (req, res) => {
+  res.redirect("/");
+});
+userRouter.get("/register", (req, res) => {
+  res.redirect("/");
+});
+
+userRouter.post("/register", async (req, res) => {
+  const { username, password: plainTextPassword } = req.body;
+
+  if (!username || typeof username !== "string") {
+    return res.json({ status: "error", error: "Invalid username" });
+  }
+
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({ status: "error", error: "Invalid password" });
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: "error",
+      error: "Password too small. Should be at least 6 characters",
+    });
+  }
+
+  const checking = await userDB.findUserByUsername({
+    username: username,
+  });
+
+  if (checking) {
+    console.log(checking.username);
+    if (checking.username === username) {
+      return res.json({ status: "error", error: "Username already in use" });
+    }
+  }
+
+  console.log("again");
+
+  const password = await bcrypt.hash(plainTextPassword, 10);
+
+  try {
+    const response = await userDB.createUser({
+      username,
+      password,
+    });
+    console.log("User created successfully: ", response);
+  } catch (error) {
+    if (error.code === 11000) {
+      // duplicate key
+      return res.json({ status: "error", error: "Username already in use" });
+    }
+    throw error;
+  }
+
+  res.json({ status: "ok" });
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userDB.findUserByUsername({ username: username });
+
+  if (!user) {
+    return res.json({ status: "error", error: "Invalid username/password" });
+  }
+
+  if (await bcrypt.compare(password, user.password)) {
+    console.log("correct password");
+    // the username, password combination is successful
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      JWT_SECRET
+    );
+
+    return res.json({ status: "ok", data: token });
+  }
+
+  res.json({ status: "error", error: "Invalid username/password" });
+});
+
+export default userRouter;
