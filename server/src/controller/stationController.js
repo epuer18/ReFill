2326e316be3e5f003ef5b5d5aabfb14dbb64ec44
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-08 07:06:54 +0000

    update station controller

--- server/src/controller/stationController.js
+++ server/src/controller/stationController.js
@@ -1,2 +1,20 @@
 import connectDB from "../db/db.js";
 import { ObjectId } from "mongodb";
+
+function stationDb() {
+    const me = {};
+    me.createStation = async (station) => {
+      const { client, collection } = await connectDB("stations");
+      const res = await collection.insertOne({
+        location: station.location,
+        description: station.description,
+      });
+      try {
+        return res;
+      } finally {
+        await client.close();
+      }
+    };
+
+    
+}
