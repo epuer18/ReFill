2a88c35943eb7cc6de408928d3a4a8fe69b7bb92
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-09 08:22:03 +0000

    debuging DB connection

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const username = encodeURIComponent("qiwenxin98");
-const password = encodeURIComponent("Zjjxwjp");
+// const username = encodeURIComponent("qiwenxin98");
+// const password = encodeURIComponent("Zjjxwjp");
 
 const connectDB = async (collectionChosen) => {
+<<<<<<< Updated upstream
   const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
+=======
+  const mongoUrl =
+    "mongodb+srv://qiwenxin98:Zjjxwjp@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
+>>>>>>> Stashed changes
   const client = new MongoClient(mongoUrl);
   await client.connect();
   const db = await client.db("Refill");
