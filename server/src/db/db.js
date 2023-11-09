14324fff8f3fe518cd073cf1cef6fc46754d0934
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-09 20:59:37 +0000

    fix the credentials leaking

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,14 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const username = encodeURIComponent("qiwenxin98");
-// const password = encodeURIComponent("Zjjxwjp");
-
 const connectDB = async (collectionChosen) => {
-
-  const mongoUrl =
-    "mongodb+srv://qiwenxin98:Zjjxwjp@cluster0.chnfjby.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(mongoUrl);
+  // const mongoUrl = process.env.MONGO_URL;
+  // console.log(process.env.MONGO_URL);
+  const client = new MongoClient(process.env.MONGO_URL);
   await client.connect();
   const db = await client.db("Refill");
   const collection = db.collection(collectionChosen);
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import path, {dirname} from 'path';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./route/userRoute.js";
 import apiRouter from "./route/apiRouter.js";
 import bodyParser from "body-parser";
-import logger from 'morgan';
+import logger from "morgan";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,14 +13,14 @@ const __dirname = dirname(__filename);
 
 let app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.use("/auth", userRouter);
 app.use("/api", apiRouter);
