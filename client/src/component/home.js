af19a17995432da35ac51f38c63c77cc99b93a69
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-11-11 01:24:10 +0000

    modify the home page

--- client/src/component/home.js
+++ client/src/component/home.js
@@ -3,25 +3,30 @@ import { Link } from "react-router-dom";
 import "../css/home.css";
 
 const Home = () => {
-  return <div className="welcome_text"> 
-    <h1> <strong>Welcome to ReFill: The Hydration Application</strong> </h1>
-    <br/>
-    <div className="mb-5"> 
-      Our goal is to connect you with accessible water bottle filling stations, or log in to add your own!
+  return (
+    <div className="welcome_text">
+      <h1>
+        {" "}
+        <strong>Welcome to ReFill: The Hydration Application</strong>{" "}
+      </h1>
+      <div className="mb-5">
+        Our goal is to connect you with accessible water bottle filling
+        stations, or log in to add your own!
+      </div>
+      <section>
+        <div>
+          <Link to="/stations" className="btn btn-primary mb-4">
+            Begin Exploring
+          </Link>
+        </div>
+        <div>
+          <Link to="/add" className="btn btn-primary mb-4">
+            Log a Station
+          </Link>
+        </div>
+      </section>
     </div>
-    <section>
-      <div>
-      <Link to="/stations" className='btn btn-primary mb-2 co'>
-        Begin Exploring
-      </Link>
-      </div>
-      <div>
-      <Link to="/add" className='btn btn-primary mb-2'>
-        Log a Station
-      </Link>
-      </div>
-    </section>
-  </div>;
+  );
 };
 
 export default Home;
