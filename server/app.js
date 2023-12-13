88e9028c003bdb6730d3ffb8545998643b01320a
Wenxin7 <92330696+Wenxin7@users.noreply.github.com>
2023-12-13 13:26:37 -0800

    update using passport & useEffect

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ import apiRouter from "./route/apiRouter.js";
 import bodyParser from "body-parser";
 import logger from "morgan";
 import { fileURLToPath } from "url";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import session from "express-session";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -21,6 +24,49 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await userDB.findUserByUsername({ username: username });
+
+      if (!user) {
+        return done(null, false, { message: "Invalid username/password" });
+      }
+
+      const isValid = await crypto.compare(password, user.password);
+
+      if (!isValid) {
+        return done(null, false, { message: "Invalid username/password" });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+app.use(
+  session({
+    secret: "Jill secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  process.nextTick(function () {
+    done(null, { id: user._id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function (user, done) {
+  process.nextTick(function () {
+    return done(null, user);
+  });
+});
+app.use(passport.authenticate("session"));
 
 app.use("/", userRouter);
 app.use("/", apiRouter);
